4fcceeaaa13ee2b75fc3f4cf803334ba354946b8
Adson Leal <adsonpleal@gmail.com>
2026-02-27 19:52:55 -0300

    feat: add open polyline measurement

--- lib/src/tools/measure_tool.dart
+++ lib/src/tools/measure_tool.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/widgets.dart';
 
 import '../constants/arcadia_color.dart';
+import '../foundation/geometry/measurement_math.dart';
+import '../foundation/units/metric_value_format.dart';
+import '../geometry/line.dart';
 import 'tool.dart';
 
 /// A tool that measures temporary polylines without committing geometry.
@@ -22,11 +25,69 @@ class MeasureTool implements Tool {
 }
 
 class _MeasureToolAction extends ToolAction {
-  @override
-  void onClickUp() {}
+  final List<Offset> _points = [];
 
   @override
-  void onCursorPositionChange() {}
+  void onClickUp() {
+    _points.add(state.cursorPosition);
+    _updatePreview();
+    _updateMeasureLabel();
+  }
+
+  @override
+  void onCursorPositionChange() {
+    _updatePreview();
+    _updateMeasureLabel();
+  }
+
+  @override
+  void onCancel() {
+    _points.clear();
+    clearToolGeometries();
+    clearMeasureLabel();
+  }
+
+  List<Offset> get _previewPoints {
+    if (_points.isEmpty) {
+      return const [];
+    }
+
+    if (state.cursorPosition == _points.last) {
+      return _points;
+    }
+
+    return [..._points, state.cursorPosition];
+  }
+
+  void _updatePreview() {
+    clearToolGeometries();
+
+    final previewPoints = _previewPoints;
+    if (previewPoints.length < 2) {
+      return;
+    }
+
+    addToolGeometries([
+      for (var i = 1; i < previewPoints.length; i++)
+        Line(
+          start: previewPoints[i - 1],
+          end: previewPoints[i],
+          color: .primary,
+        ),
+    ]);
+  }
+
+  void _updateMeasureLabel() {
+    final previewPoints = _previewPoints;
+    if (previewPoints.length < 2) {
+      clearMeasureLabel();
+      return;
+    }
+
+    setMeasureLabel(
+      'Length: ${formatMetricLength(polylineLength(previewPoints), state.selectedUnit)}',
+    );
+  }
 }
 
 class _MeasureToolIcon extends StatelessWidget {
--- test/src/tools/measure_tool_test.dart
+++ test/src/tools/measure_tool_test.dart
@@ -1,3 +1,6 @@
+import 'package:arcadia/src/constants/config.dart';
+import 'package:arcadia/src/geometry/line.dart';
+import 'package:arcadia/src/logic/viewport_notifier.dart';
 import 'package:arcadia/src/tools/measure_tool.dart';
 import 'package:arcadia/src/tools/tool.dart';
 import 'package:flutter/widgets.dart';
@@ -13,5 +16,38 @@ void main() {
       expect(tool.icon, isA<Widget>());
       expect(tool.toolActionFactory(), isA<ToolAction>());
     });
+
+    test('shows running polyline length without committing geometry', () {
+      final notifier = ViewportNotifier()..selectTool(const MeasureTool());
+
+      _moveCursor(notifier, const Offset(10, 10));
+      notifier.onCursorClickUp();
+      _moveCursor(notifier, const Offset(20, 10));
+
+      final firstPreview = notifier.value.toolGeometries.single as Line;
+      expect(firstPreview.start, const Offset(10, 10));
+      expect(firstPreview.end, const Offset(20, 10));
+      expect(notifier.value.measureLabel, 'Length: 10.0 mm');
+      expect(notifier.value.geometries, isEmpty);
+
+      notifier.onCursorClickUp();
+      _moveCursor(notifier, const Offset(20, 20));
+
+      final chainedPreview = notifier.value.toolGeometries.whereType<Line>().toList();
+      expect(chainedPreview, hasLength(2));
+      expect(chainedPreview.first.start, const Offset(10, 10));
+      expect(chainedPreview.first.end, const Offset(20, 10));
+      expect(chainedPreview.last.start, const Offset(20, 10));
+      expect(chainedPreview.last.end, const Offset(20, 20));
+      expect(notifier.value.measureLabel, 'Length: 20.0 mm');
+      expect(notifier.value.geometries, isEmpty);
+    });
   });
+}
+
+void _moveCursor(ViewportNotifier notifier, Offset cursorPosition) {
+  notifier.onCursorMove(
+    viewportPosition: cursorPosition * unitVirtualPixelRatio,
+    viewportMidPoint: .zero,
+  );
 }
